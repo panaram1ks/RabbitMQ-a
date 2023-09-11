141fb9a5fbba1f5983a549fded10a7ed0007e062
parom <E.Parominskiy@isida.by>
2023-09-11 15:30:32 +0300

    Create Exchanges with rabbitMQ

---
+++ rabbitmqlistener/src/main/java/com/example/rabbitmqlistener/RabbitMQExchangeConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.rabbitmqlistener;
+
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQExchangeConfiguration {
+
+    @Bean
+    Exchange exchangeExchange() {
+        return new TopicExchange("ExampleExchange");
+    }
+
+    @Bean
+    Exchange exchange2ndExchange() {
+        return ExchangeBuilder.directExchange("Exchange2ndExchange")
+                .autoDelete()
+                .internal()
+                .build();
+    }
+}
