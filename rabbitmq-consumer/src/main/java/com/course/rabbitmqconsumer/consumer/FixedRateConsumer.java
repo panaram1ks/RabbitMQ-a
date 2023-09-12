e32f3fde0b9ea173936fb0169864b8ec27c3f43d
parom <E.Parominskiy@isida.by>
2023-09-12 16:24:51 +0300

    start many consumers to get all messages faster

--- rabbitmq-consumer/src/main/java/com/course/rabbitmqconsumer/consumer/FixedRateConsumer.java
+++ rabbitmq-consumer/src/main/java/com/course/rabbitmqconsumer/consumer/FixedRateConsumer.java
@@ -5,14 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class FixedRateConsumer {
 
     public static final Logger LOG = LoggerFactory.getLogger(FixedRateConsumer.class);
 
-    @RabbitListener(queues = "course.fixedrate")
-    public void messaging(String message) {
-        LOG.info("Consuming {}", message);
+    @RabbitListener(queues = "course.fixedrate", concurrency = "3-7")
+    public void messaging(String message) throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextLong(1000, 2000));
+        LOG.info("{} : Consuming {}", Thread.currentThread().getName(), message);
     }
 
 }
