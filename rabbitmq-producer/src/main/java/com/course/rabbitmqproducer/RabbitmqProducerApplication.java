c973c4272fde4945ddfef68f13733d1f1bb93ba7
parom <E.Parominskiy@isida.by>
2023-09-12 14:21:50 +0300

    rabbitmq-producer sendMessage

--- rabbitmq-producer/src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
+++ rabbitmq-producer/src/main/java/com/course/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -1,13 +1,25 @@
 package com.course.rabbitmqproducer;
 
+import com.course.rabbitmqproducer.producer.HelloRabbitProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitmqProducerApplication {
+public class RabbitmqProducerApplication implements CommandLineRunner {
+
+	public HelloRabbitProducer helloRabbitProducer;
+
+	public RabbitmqProducerApplication(HelloRabbitProducer helloRabbitProducer) {
+		this.helloRabbitProducer = helloRabbitProducer;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqProducerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		helloRabbitProducer.sendHello("Timotius Pamungkas " + Math.random());
+	}
 }
