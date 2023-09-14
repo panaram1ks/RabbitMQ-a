fc9154332df40b7d2724a3a64fbbe236cbd4adf2
parom <E.Parominskiy@isida.by>
2023-09-14 17:46:55 +0300

    ProducerTwo

---
+++ rabbitmq.producer.two/src/test/java/com/parom/rabbitmq/producer/two/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parom.rabbitmq.producer.two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
