81d13d37770a6c0eaf5f6380e7fb0c469b7d19cc
parom <E.Parominskiy@isida.by>
2023-09-14 08:58:08 +0300

    Handle Consumer Exception with DLX (Solution Two)

--- rabbitmq-consumer/src/main/java/com/course/rabbitmqconsumer/consumer/MyPictureImageConsumer.java
+++ rabbitmq-consumer/src/main/java/com/course/rabbitmqconsumer/consumer/MyPictureImageConsumer.java
@@ -2,11 +2,14 @@ package com.course.rabbitmqconsumer.consumer;
 
 import com.course.rabbitmqconsumer.entity.Picture;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -20,13 +23,17 @@ public class MyPictureImageConsumer {
     private static final Logger LOG = LoggerFactory.getLogger(MyPictureImageConsumer.class);
 
     @RabbitListener(queues = "q.mypicture.image")
-    public void listen(String message) throws IOException, IllegalAccessException {
+    public void listen(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException, IllegalAccessException {
         var picture = objectMapper.readValue(message, Picture.class);
         if (picture.getSize() > 9000) {
-            throw new AmqpRejectAndDontRequeueException("Picture size too large : " + picture);
+//            throw new AmqpRejectAndDontRequeueException("Picture size too large : " + picture);
+            channel.basicReject(tag, false);
+            return;
+
         }
 
         LOG.info("Picture type:{} , consumer:{}", picture.getType(), MyPictureImageConsumer.class.getSimpleName());
+        channel.basicAck(tag, false);
     }
 
 }
