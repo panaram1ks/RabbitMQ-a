716b5871956eedd358e6fde25d44e06b697b8912
parom <E.Parominskiy@isida.by>
2023-09-15 15:23:25 +0300

    Create Exchanges & Queues From Java Code second way

--- rabbitmq.producer.two/src/main/java/com/parom/rabbitmq/two/config/RabbitmqSchemaConfig.java
+++ rabbitmq.producer.two/src/main/java/com/parom/rabbitmq/two/config/RabbitmqSchemaConfig.java
@@ -7,25 +7,36 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitmqSchemaConfig {
 
-    @Bean
-    public FanoutExchange fanoutExchange() {
-        return new FanoutExchange("x.another-dummy", true, false);
-    }
+//    @Bean
+//    public FanoutExchange fanoutExchange() {
+//        return new FanoutExchange("x.another-dummy", true, false);
+//    }
+//
+//    @Bean
+//    public DirectExchange directExchange() {
+//        return new DirectExchange("test");
+//    }
+//
+//    @Bean
+//    public Queue queueAnotherDummy() {
+//        return new Queue("q.another-dummy");
+//    }
+//
+//    @Bean
+//    public Binding bindingAnotherDummy() {
+//        // first variant to create Binding
+////        return new Binding("q.another-dummy", Binding.DestinationType.QUEUE, "x.another-dummy", "", null);
+//        return BindingBuilder.bind(queueAnotherDummy()).to(fanoutExchange());
+//    }
 
-    @Bean
-    public DirectExchange directExchange() {
-        return new DirectExchange("test");
-    }
 
-    @Bean
-    public Queue queueAnotherDummy() {
-        return new Queue("q.another-dummy");
-    }
 
-    @Bean
-    public Binding bindingAnotherDummy() {
-        // first variant to create Binding
-//        return new Binding("q.another-dummy", Binding.DestinationType.QUEUE, "x.another-dummy", null, null);
-        return BindingBuilder.bind(queueAnotherDummy()).to(fanoutExchange());
+    @Bean // second way create all in one
+    public Declarables rabbitmqSchema() {
+        return new Declarables(
+                new FanoutExchange("x.another-dummy", true, false),
+                new Queue("q.another-dummy"),
+                new Binding("q.another-dummy", Binding.DestinationType.QUEUE, "x.another-dummy", "", null)
+        );
     }
 }
